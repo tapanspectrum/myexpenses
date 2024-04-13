b452b9bb4b2ad6a0ab790b1af96d8c9bd3229f7b
Tapan Acharjee <tapanspectrum@gmail.com>
2024-04-13 20:16:40 +0530

    deployment code updated

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,9 @@
 git pull;
-pm2 stop ngapp;
+# pm2 stop ngapp;
 npm i ;
 # pm2 start "ng start" --name "ngapp";
-pm2 restart ngapp;
+# pm2 restart ngapp;
+
+npm run build;
+
+sudo cp .  -r /home/fe-dailyexpenses/myexpenses/dist/tapu-product/ /var/www/html/prod
